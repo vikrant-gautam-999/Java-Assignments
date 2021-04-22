fd5d807167dbcf02322284c1206ee711b710912c
vikrant-gautam-999 <78501213+vikrant-gautam-999@users.noreply.github.com>
2021-04-22 12:35:46 +0530

    Delete 9.1. IdComparator.java

--- 9.1. IdComparator.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class IdComparator implements Comparator<Subject2>
-{
-	public int compare(Subject2 user1, Subject2 user2)
-	{
-		return user1.getId() - user2.getId();
-	}
-}
