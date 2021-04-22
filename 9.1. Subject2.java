fd38b4bcea19d778b942d5c80de8bb462000b3ff
vikrant-gautam-999 <78501213+vikrant-gautam-999@users.noreply.github.com>
2021-04-22 12:34:04 +0530

    Add files via upload

---
+++ 9.1. Subject2.java
@@ -0,0 +1,31 @@
+
+public class Subject2
+{
+	int id;
+	String name;
+	
+	public Subject2(int id, String name){
+		this.id = id;
+		this.name = name;
+	}
+	
+	public int getId(){
+		return id;
+	}
+	
+	public void setId(int id){
+		this.id = id;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String toString(){
+		return id + " : " + name;
+	}
+}
