c5aee8b560ccb20aff796f1ba7dbf4e109f57630
vikrant-gautam-999 <78501213+vikrant-gautam-999@users.noreply.github.com>
2021-04-22 12:40:48 +0530

    Add files via upload

---
+++ 9.2.2. SortUsingComparable.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.util.Collections;
+import java.util.List;
+
+public class SortUsingComparable
+{
+	public static void main(String[] args){
+		List<Subject> course = new ArrayList<Subject>();
+		Subject user1 = new Subject(161, "Node.js");
+		Subject user2 = new Subject(108, "Angular.js");
+		Subject user3 = new Subject(119, "Spring");
+		Subject user4 = new Subject(156, "Java");
+		Subject user5 = new Subject(56, "Notepad++");
+		
+		course.add(user1);
+		course.add(user2);
+		course.add(user3);
+		course.add(user4);
+		course.add(user5);
+		
+		System.out.println("Unsorted ArrayList");
+		for(Subject user : course)
+		{
+			System.out.println(user);
+		}
+		
+		Collections.sort(course);
+		
+		System.out.println();
+		System.out.println("Sorted ArrayList");
+		for(Subject user : course)
+		{
+			System.out.println(user);
+		}
+	}
+}
